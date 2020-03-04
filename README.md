a044819db9b222d4108b50acfa56f2658f818ecb
Arianna Zanetti <guszanear@GU.GU.SE@mltgpu.flov.gu.se>
2020-03-04 11:13:09 +0100

    Assignment 2.

--- README.md
+++ README.md
@@ -1,4 +1,198 @@
 # LT2212 V20 Assignment 2
 
-Put any documentation here including any answers to the questions in the 
-assignment on Canvas.
+Part 1 - creating the feature table
+
+The function "extract_features(samples)" loops through the given samples and selects the words to use as features.
+First, it lower-cases each word, then it excludes the numbers and removes punctuation marks from the remaining strings.
+When the feature table is created, it removes unfrequent words, in particular words that do not appear more than 5 times in any of the documents, and words that appear in more than half of the documents (likely to be stop-words).
+The number of selected features after the filter is: 6820.
+
+
+Part 3 - classify and evaluate 
+
+--model_id = 1 Creates a Support Vector Classifier
+--model_id = 2 Creates a Decision Tree Classifier
+
+
+Part 4 - try and discuss
+
+1.  SVC:
+
+	Accuracy: 0.6153846153846154
+	Precision: 0.7312835620468641
+	Recall: 0.6153846153846154
+	F-measure: 0.6295393242562133
+
+
+	Decision tree:
+
+	Accuracy: 0.6289124668435013
+	Precision: 0.6345327387873417
+	Recall: 0.6289124668435013
+	F-measure: 0.630396825430384
+	
+	
+2.  50% (3000 features)
+
+	SVC:
+
+	Accuracy: 0.6647214854111406
+	Precision: 0.7450556008221445
+	Recall: 0.6647214854111406
+	F-measure: 0.6742866308812862
+
+
+	Decision tree:
+
+	Accuracy: 0.2339522546419098
+	Precision: 0.23712542693859126
+	Recall: 0.2339522546419098
+	F-measure: 0.23484380138558344
+	
+	
+	25% (1500 features)
+
+	SVC:
+
+	Accuracy: 0.6814323607427055
+	Precision: 0.7434391482719998
+	Recall: 0.6814323607427055
+	F-measure: 0.6884433865720662
+
+
+	Decision tree:
+
+	Accuracy: 0.24350132625994694
+	Precision: 0.2427450709627398
+	Recall: 0.24350132625994694
+	F-measure: 0.24269421907822789
+		
+	
+	10% (600 features)
+
+	SVC:
+
+	Accuracy: 0.6787798408488064
+	Precision: 0.7230790830118445
+	Recall: 0.6787798408488064
+	F-measure: 0.6830548214650765
+
+
+	Decision tree:
+
+	Accuracy: 0.2509283819628647
+	Precision: 0.25457217698524215
+	Recall: 0.2509283819628647
+	F-measure: 0.25203599577295216
+		
+	
+	5% (300 features)
+
+	SVC:
+
+	Accuracy: 0.6429708222811671
+	Precision: 0.6861023190984168
+	Recall: 0.6429708222811671
+	F-measure: 0.6466920622707599
+
+
+	Decision tree:
+
+	Accuracy: 0.2572944297082228
+	Precision: 0.26141784913178323
+	Recall: 0.2572944297082228
+	F-measure: 0.2584856114475635
+	
+	
+What we can see from the results is that the Support Vector Classifier has similar values of precision and recall with the reduced features, around 65%/70% on average. 
+It seems to even work a bit better when the features are 25% or even 10% of the original number, even if it could just be chance.
+The decision tree classifier, on the contrary, has comparable values of precision and recall to SVC when used on the complete training set (62% and 63%), but they are drastically decreased to 23%/24% when used on the reduced features, already with 50% of the data and reducing even more does not make a big difference.
+
+
+Part Bonus - another dimensionality reduction
+
+In part 2, I used Principal component analysis for dimensionality reduction, so in part bonus I decided to use Truncated SVD.
+To use the alternative dimensionality reduction, the script has to be called with the additional parameter -r (--alternative_reduction).
+
+Results:
+
+	50% (3000 features)
+
+	SVC:
+
+	Accuracy: 0.6633952254641909
+	Precision: 0.7439853719254946
+	Recall: 0.6633952254641909
+	F-measure: 0.6728091934627611
+
+
+	Decision tree:
+
+	Accuracy: 0.23474801061007958
+	Precision: 0.23694141234071014
+	Recall: 0.23474801061007958
+	F-measure: 0.23501969441285195
+	
+	
+	25% (1500 features)
+
+	SVC:
+
+	Accuracy: 0.6816976127320955
+	Precision: 0.7436190781941717
+	Recall: 0.6816976127320955
+	F-measure: 0.6888051423045148
+	
+
+	Decision tree:
+	
+	Accuracy: 0.2440318302387268
+	Precision: 0.2466419643714602
+	Recall: 0.2440318302387268
+	F-measure: 0.24437638774067905
+
+
+	10% (600 features)
+
+	SVC:
+
+	Accuracy: 0.6779840848806366
+	Precision: 0.7233266660870354
+	Recall: 0.6779840848806366
+	F-measure: 0.681847270332927
+	
+
+	Decision tree:
+
+	Accuracy: 0.24297082228116712
+	Precision: 0.24613071351205032
+	Recall: 0.24297082228116712
+	F-measure: 0.24385754232075632
+
+	
+	5% (300 features)
+
+	SVC:
+
+	Accuracy: 0.6456233421750663
+	Precision: 0.6864301485343208
+	Recall: 0.6456233421750663
+	F-measure: 0.6481380846632142
+	
+
+	Decision tree:
+	
+	Accuracy: 0.259946949602122
+	Precision: 0.26156888670090034
+	Recall: 0.259946949602122
+	F-measure: 0.2599534294092134
+
+	
+With the alternative dimensionality reduction, we can confirm our observations: the Support Vector Classifier maintains values of precision and recall between 65%/70% on average, while the Decision Tree Classifier doesn't work well with any kind of dimensionality reduction, having values of precision and recall around 24%/25%.
+
+
+	
+
+  
+	
+	
--- a2.py
+++ a2.py
@@ -1,13 +1,19 @@
 import argparse
 import random
-from sklearn.datasets import fetch_20newsgroups
+from collections import Counter
 from sklearn.base import is_classifier
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD
+import sklearn.metrics as metrics
+import math
 import numpy as np
+import string
 random.seed(42)
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
 
 
-###### PART 1
-#DONT CHANGE THIS FUNCTION
 def part1(samples):
     #extract features
     X = extract_features(samples)
@@ -19,12 +25,34 @@ def part1(samples):
 
 def extract_features(samples):
     print("Extracting features ...")
-    pass #Fill this in
+    docs_list = []
+    all_words_index = {}
+    index = 0
+    for sample in samples:
+        text = sample.lower().split() # lowercase so the words are not treated differently if they appear at the beginning of a sentence.
+        text = ["".join(c for c in word if c not in string.punctuation) for word in text if not word.isnumeric()] # removes punctuation marks and numbers
+        for word in text:
+            if word not in all_words_index:
+                all_words_index[word] = index # assigns a unique index to every word in the samples
+                index += 1
+        text_dict = Counter(text)
+        text_dict = {all_words_index[word]: text_dict[word] for word in text_dict} # changes the keys to the relative index in the dictionary, so they can be used in the numpy array
+        docs_list.append(text_dict)
+    features_array = np.zeros((len(docs_list), len(all_words_index))) # creates empty array of the desired shape (one row for each document, one column for each word)
+    doc_index = 0 # creates an index for the rows
+    for doc in docs_list:
+        indexes = tuple(doc.keys())
+        features_array[doc_index][np.array(indexes)] = tuple(doc.values()) # uses advanced indexing to assign the values to the correct index
+        doc_index += 1
+    print("Done")
+    print("Removing unfrequent words ...")
+    total_docs = features_array.shape[0]
+    features_array = features_array[:, np.sum(features_array > 0, axis=0) < (total_docs / 2)] # Exclude the words that appear in more than half of the documents (probably stop-words)
+    features_array = features_array[:, np.any(features_array > 5, axis =0)] # Exclude the words that do not appear more than 5 times in any of the document (unfrequent words)
+    print("Number of words:", features_array.shape[1])
+    return features_array
 
 
-
-##### PART 2
-#DONT CHANGE THIS FUNCTION
 def part2(X, n_dim):
     #Reduce Dimension
     print("Reducing dimensions ... ")
@@ -37,18 +65,15 @@ def part2(X, n_dim):
 
 
 def reduce_dim(X,n=10):
-    #fill this in
-    pass
+    pca = PCA(n_components=n)
+    return pca.fit_transform(X)
 
 
-
-##### PART 3
-#DONT CHANGE THIS FUNCTION EXCEPT WHERE INSTRUCTED
 def get_classifier(clf_id):
     if clf_id == 1:
-        clf = "" # <--- REPLACE THIS WITH A SKLEARN MODEL
+        clf = SVC(gamma='scale')
     elif clf_id == 2:
-        clf = "" # <--- REPLACE THIS WITH A SKLEARN MODEL
+        clf = DecisionTreeClassifier()
     else:
         raise KeyError("No clf with id {}".format(clf_id))
 
@@ -56,9 +81,8 @@ def get_classifier(clf_id):
     print("Getting clf {} ...".format(clf.__class__.__name__))
     return clf
 
-#DONT CHANGE THIS FUNCTION
+
 def part3(X, y, clf_id):
-    #PART 3
     X_train, X_test, y_train, y_test = shuffle_split(X,y)
 
     #get the model
@@ -68,8 +92,8 @@ def part3(X, y, clf_id):
     print()
     print("Train example: ", X_train[0])
     print("Test example: ", X_test[0])
-    print("Train label example: ",y_train[0])
-    print("Test label example: ",y_test[0])
+    print("Train label example: ", y_train[0])
+    print("Test label example: ", y_test[0])
     print()
 
 
@@ -78,27 +102,38 @@ def part3(X, y, clf_id):
     train_classifer(clf, X_train, y_train)
 
 
-    # evalute model
-    print("Evaluating classcifier ...")
-    evalute_classifier(clf, X_test, y_test)
+    # evaluate model
+    print("Evaluating classifier ...")
+    evaluate_classifier(clf, X_test, y_test)
 
 
 def shuffle_split(X,y):
-    pass # Fill in this
+    indexes_list = list(range(X.shape[0])) # Creates a list of indexes that can be shuffled without losing the parallelism between X and y
+    random.shuffle(indexes_list)
+    last_training_index = int(0.80 * X.shape[0]) # Calculates the last index to separate the data into two parts with ratio 80/20
+    indexes_train = indexes_list[:last_training_index] # Chooses which indexes of the shuffled list will become part of the training or test set
+    indexes_test = indexes_list[last_training_index:]
+    X_train = X[indexes_train] # Creates 4 different arrays using advanced indexing
+    X_test = X[indexes_test]
+    y_train = y[indexes_train]
+    y_test = y[indexes_test]
+    return X_train, X_test, y_train, y_test
 
 
 def train_classifer(clf, X, y):
     assert is_classifier(clf)
-    ## fill in this
+    clf.fit(X, y)
 
 
-def evalute_classifier(clf, X, y):
+def evaluate_classifier(clf, X, y):
     assert is_classifier(clf)
-    #Fill this in
+    y_pred = clf.predict(X)
+    print("Accuracy:", metrics.accuracy_score(y, y_pred))
+    print("Precision:", metrics.precision_score(y, y_pred, average='weighted'))
+    print("Recall:", metrics.recall_score(y, y_pred, average='weighted'))
+    print("F-measure:", metrics.f1_score(y, y_pred,average='weighted'))
 
 
-######
-#DONT CHANGE THIS FUNCTION
 def load_data():
     print("------------Loading Data-----------")
     data = fetch_20newsgroups(subset='all', shuffle=True, random_state=42)
@@ -109,8 +144,20 @@ def load_data():
     return data.data, data.target, data.target_names
 
 
+def part_bonus(X, n_dim):
+    #Reduce Dimension
+    print("Reducing dimensions ... ")
+    svd = TruncatedSVD(n_components=n_dim)
+    X_dr = svd.fit_transform(X)
+    assert X_dr.shape != X.shape
+    assert X_dr.shape[1] == n_dim
+    print("Example sample dim. reduced feature vec: ", X[0])
+    print("Dim reduced data shape: ", X_dr.shape)
+    return X_dr
+
+
 #DONT CHANGE THIS FUNCTION
-def main(model_id=None, n_dim=False):
+def main(model_id=None, n_dim=False, alternative_reduction=False):
 
     # load data
     samples, labels, label_names = load_data()
@@ -122,8 +169,12 @@ def main(model_id=None, n_dim=False):
 
     #part 2
     if n_dim:
-        print("\n------------PART 2-----------")
-        X = part2(X, n_dim)
+        if not alternative_reduction:
+            print("\n------------PART 2-----------")
+            X = part2(X, n_dim)
+        else:
+            print("\n------------PART 2-----------")
+            X = part_bonus(X, n_dim)
 
     #part 3
     if model_id:
@@ -148,8 +199,7 @@ if __name__ == '__main__':
                         required=False,
                         help="id of the classifier you want to use")
 
+    parser.add_argument("-r", "--alternative_reduction", nargs='?', help="use alternative model for dimensionality reduction")
+        
     args = parser.parse_args()
-    main(   
-            model_id=args.model_id, 
-            n_dim=args.number_dim_reduce
-            )
+    main(model_id=args.model_id, n_dim=args.number_dim_reduce, alternative_reduction=args.alternative_reduction)
